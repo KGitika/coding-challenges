542e614e822b3172246c3b87e9f5cb1e1aa9bbc8
Gitika Kalal <gitikakalal21@gmail.com>
2024-10-01 20:11:44 -0700

    Add solution with arrays, hashtable and sorting.

---
+++ LeetCode/Mixed-topics/rank-transform-of-an-array.java
@@ -0,0 +1,45 @@
+/ Includes arrays, hashtable, and sorting;
+// Time complexity: O(m+n);
+// Space complexity: O(n);
+
+// Approch 1;
+ public int[] arrayRankTransform(int[] arr) {
+        // Store the rank for each number in arr
+        HashMap<Integer, Integer> numToRank = new HashMap<>();
+        int[] sortedArr = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sortedArr);
+        int rank = 1;
+        for (int i = 0; i < sortedArr.length; i++) {
+            if (i > 0 && sortedArr[i] > sortedArr[i - 1]) {
+                rank++;
+            }
+            numToRank.put(sortedArr[i], rank);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = numToRank.get(arr[i]);
+        }
+        return arr;
+    }
+
+
+// Approch 2
+public int[] arrayRankTransform(int[] arr) {
+
+         TreeMap<Integer, List<Integer>> numToIndices = new TreeMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!numToIndices.containsKey(arr[i])) {
+                numToIndices.put(arr[i], new ArrayList<>());
+            }
+            numToIndices.get(arr[i]).add(i);
+        }
+        int rank = 1;
+        for (int num : numToIndices.keySet()) {
+            for (int index : numToIndices.get(num)) {
+                arr[index] = rank;
+            }
+            rank++;
+        }
+        return arr;
+    }
+}
