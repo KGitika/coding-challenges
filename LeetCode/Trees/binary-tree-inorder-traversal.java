0ef45d2fccd9fa605bfaca7c579daa584d6505e6
Gitika Kalal <gitikakalal21@gmail.com>
2024-10-08 21:30:22 -0700

    Add binary-tree-inorder-traversal

---
+++ LeetCode/Trees/binary-tree-inorder-traversal.java
@@ -0,0 +1,51 @@
+// Problem: binary-tree-inorder-traversal
+// URL: https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+// Approach 1: Recursive Approach
+// Time Complexity: O(n);
+// Space Complexity: O(n);
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+        }
+    }
+}
+
+// Approach 2: Morris Traversal
+// Time Complexity: O(n);
+// Space Complexity: O(1);
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+        TreeNode pre;
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right; // move to next right node
+            } else { // has a left subtree
+                pre = curr.left;
+                while (pre.right != null) { // find rightmost
+                    pre = pre.right;
+                }
+                pre.right = curr; // put cur after the pre node
+                TreeNode temp = curr; // store cur node
+                curr = curr.left; // move cur to the top of the new tree
+                temp.left = null; // original cur left be null, avoid infinite loops
+            }
+        }
+        return res;
+    }
+}
