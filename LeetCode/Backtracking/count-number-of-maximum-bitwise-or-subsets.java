f35746bb17ef9e1f7092e6bb63962a01b87e9597
Gitika Kalal <gitikakalal21@gmail.com>
2024-10-17 20:42:54 -0700

    Add Backtracking Solution

---
+++ LeetCode/Backtracking/count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,43 @@
+// Problem: Count Number of Maximum Bitwise-OR Subsets
+// URL: https://leetcode.com/count-number-of-maximum-bitwise-or-subsets/
+
+
+// Time Complexity: O(2^n);
+// Space Complexity: O(n);
+
+class Solution {
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOrValue = 0;
+        for (int num : nums) {
+            maxOrValue |= num;
+        }
+        return countSubsets(nums, 0, 0, maxOrValue);
+    }
+
+    private int countSubsets(
+        int[] nums,
+        int index,
+        int currentOr,
+        int targetOr
+    ) {
+        // Base case: reached the end of the array
+        if (index == nums.length) {
+            return (currentOr == targetOr) ? 1 : 0;
+        }
+
+        // Don't include the current number
+        int countWithout = countSubsets(nums, index + 1, currentOr, targetOr);
+
+        // Include the current number
+        int countWith = countSubsets(
+            nums,
+            index + 1,
+            currentOr | nums[index],
+            targetOr
+        );
+
+        // Return the sum of both cases
+        return countWithout + countWith;
+    }
+}
