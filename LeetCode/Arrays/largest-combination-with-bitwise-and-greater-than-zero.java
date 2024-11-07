0f36537eab97a91547c502ff53e50916956f40b2
Gitika Kalal <gitikakalal21@gmail.com>
2024-11-06 22:53:15 -0800

    Add Largest Combination With Bitwise AND Greater Than Zero

---
+++ LeetCode/Arrays/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -0,0 +1,20 @@
+// Problem: Largest Combination With Bitwise AND Greater Than Zero
+
+// URL: https://leetcode.com/largest-combination-with-bitwise-and-greater-than-zero/
+
+class Solution {
+
+    public int largestCombination(int[] candidates) {
+        int maxCount = 0; // Variable to track the maximum count of set bits.
+        for (int i = 0; i < 24; i++) {
+            int count = 0; // Count of numbers with the i-th bit set.
+            for (int num : candidates) {
+                if ((num & (1 << i)) != 0) { // Check if the i-th bit is set.
+                    count++;
+                }
+            }
+            maxCount = Math.max(maxCount, count); // Update the maximum count.
+        }
+        return maxCount;
+    }
+}
